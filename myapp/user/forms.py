6991515d2f130d667407b6cba4634a39d1ee580b
hayoung lee <hayoung0.lee@gmail.com>
2019-09-02 22:27:41 +0900

    마감기한 기능

--- myapp/user/forms.py
+++ myapp/user/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from .models import Todo
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+    
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ('title', 'content')
-
+        fields = ('title', 'content', 'due_date')
+        widgets = {
+            'due_date': DateInput(attrs={'type': 'date'})
+        }
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- myapp/user/models.py
+++ myapp/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class User(models.Model):
@@ -8,11 +9,12 @@ class User(models.Model):
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    due_date = models.DateField(("Date"), default=datetime.date.today)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.created_date = timezone.now()
         self.save()
 
     def __str__(self):
