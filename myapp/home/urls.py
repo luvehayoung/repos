f8512d1c65b5dc6f58be8d990dc30a8a03a2e1dc
hayoung lee <hayoung0.lee@gmail.com>
2019-09-02 21:54:18 +0900

    수정 삭제 기능까지 추가

--- myapp/home/urls.py
+++ myapp/home/urls.py
@@ -5,4 +5,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('todo/create', views.createTodo, name = 'createTodo'),
+    path('todo/view/<int:pk>', views.viewTodo, name = 'viewTodo'),
+    path('todo/edit/<int:pk>', views.editTodo, name='editTodo'),
+    path('todo/delete/<int:pk>', views.deleteTodo, name='deleteTodo'),
 ]
--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from myapp.user.models import Todo
 from myapp.user.forms import TodoForm
 # Create your views here.
@@ -6,15 +6,34 @@ def index(request):
     todo = Todo.objects.order_by('created_at').reverse()
     return render(request, 'index.html', {'todos':todo})
 
-
 def createTodo(request):
     if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
+            todo = form.save(commit=False)
+            todo.save()
             return redirect('index')
         return render(request, 'createTodo.html')
     else:
         form = TodoForm()
         return render(request, 'createTodo.html', {'form':form})
+
+def viewTodo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    return render(request, 'viewTodo.html', {'todo':todo})
+
+def editTodo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save()
+            return redirect('viewTodo', pk=todo.pk)
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'createTodo.html', {'form':form})
+
+def deleteTodo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect('index')
